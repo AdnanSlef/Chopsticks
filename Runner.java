1fbba33e25c41b047d27314498b692c0fe9305a4
Adrian Self <aself.school@gmail.com>
2020-07-13 16:41:45 -0400

    Create runner

---
+++ Runner.java
@@ -0,0 +1,18 @@
+/**
+ * Runs the Chopsticks game
+ *
+ * @author Adrian Self
+ * @version V0.0.0, 07/13/2020
+ */
+public class Runner
+{
+    private static void Test1()
+    {
+        Game game = new Game();
+    }
+    
+    public static void main(String[] args)
+    {
+        Test1();
+    }
+}
